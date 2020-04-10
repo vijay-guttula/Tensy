461cdb44e24c7dc0535b19fd16718b8d1a748267
vijay-guttula <ssvijayg@gmail.com>
2020-04-10 18:30:19 +0530

    First Commit

---
+++ inference.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import numpy as np
+import json
+import requests
+
+SIZE = 128 #this model requires the size as 128 by 128
+MODEL_URI = 'http://localhost:8501/v1/models/pets:predict'
+CLASSES = ['Cat','Dog']
+
+""" This method takes the image and processes it into the required dimensions"""
+def get_prediction(image_path):
+    image = tf.keras.preprocessing.image.load_img(
+        image_path, target_size = (SIZE,SIZE)
+    )
+    #coverts the image to numpy array
+    image = tf.keras.preprocessing.image.img_to_array(image)
+    image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
+    image = np.expand_dims(image, axis=0)
+    
+
+    data = json.dumps({
+        'instances': image.tolist()
+    })
+    
+    response = requests.post(MODEL_URI, data=data.encode()) #as this will return a response
+
+    result = json.loads(response.text)
+    #return resultpip 
+    #print(result)
+    prediction = np.squeeze(result['predictions'][0])
+    class_name = CLASSES[int(prediction > 0.5)]
+    return class_name
+
+#print(get_prediction('dog.jpeg'))
